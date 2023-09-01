df90a5c1db5e1861f1f60d4805d6ba3cead1ea69
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-09-01 19:52:45 +0300

    fix

---
+++ src/js/fn-request-api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+async function serviceSearch(inputValue, currentPage) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '39170580-8c73c06b0fce5ecb6f9d6ab10',
+    q: inputValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: '40',
+    page: currentPage,
+  });
+  return await axios.get(`${BASE_URL}?${params}`);
+}
+
+async function serviceSearchLoadMore(currentPage) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '39170580-8c73c06b0fce5ecb6f9d6ab10',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: '40',
+    page: currentPage,
+  });
+  return await axios.get(`${BASE_URL}?${params}`);
+}
+
+export { serviceSearch, serviceSearchLoadMore };
